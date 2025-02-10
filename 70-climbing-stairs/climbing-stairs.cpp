7bfb10b11ab1a20c788f86fa85a631bd265a79d3
Aman Singh Negi <159263291+aman-singh-negi@users.noreply.github.com>
2025-02-10 15:23:34 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (35.09%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
-    int calculate(int n,vector<int>&v)
-    {
-        if(n<=2)return n;
-        if(v[n]!=-1)return v[n];
-        v[n]=calculate(n-1,v)+calculate(n-2,v);
-        return v[n];
-    }
     int climbStairs(int n) {
-        //Memoization
+        //Tabular
+        if(n<=2)return n;
         vector<int>v(n+1,-1);
-        return calculate(n,v);
+        v[0]=0;
+        v[1]=1;
+        v[2]=2;
+        for(int i=3;i<=n;i++)
+        {
+            v[i]=v[i-1]+v[i-2];
+        }
+        return v[n];
     }
 };
