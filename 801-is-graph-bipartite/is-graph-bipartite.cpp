7c8d2d889a80b40e2ccbfddaac76ae76cf135637
Aman Singh Negi <159263291+aman-singh-negi@users.noreply.github.com>
2025-03-15 22:34:13 +0530

    Time: 0 ms (100.00%) | Memory: 17.2 MB (76.75%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.cpp
+++ 801-is-graph-bipartite/is-graph-bipartite.cpp
@@ -1,23 +1,21 @@
 class Solution {
 private:
     bool dfs(vector<vector<int>>& graph, vector<int>& visited, int src) {
-        for (int neighbor : graph[src]) {
-            if (visited[neighbor] == -1) {
-                visited[neighbor] = 1 - visited[src];
-                if (!dfs(graph, visited, neighbor)) return false;
+        for (int i=0;i<graph[src].size();i++) {
+            if (visited[graph[src][i]] == -1) {
+                visited[graph[src][i]] = 1 - visited[src];
+                if (!dfs(graph, visited, graph[src][i])) return false;
             } 
-            else if (visited[neighbor] == visited[src]) {
+            else if (visited[graph[src][i]] == visited[src]) {
                 return false;
             }
         }
         return true;
     }
-
 public:
     bool isBipartite(vector<vector<int>>& graph) {
         int n = graph.size();
         vector<int> visited(n, -1);
-
         for (int i = 0; i < n; i++) {
             if (visited[i] == -1) { 
                 visited[i] = 0;
