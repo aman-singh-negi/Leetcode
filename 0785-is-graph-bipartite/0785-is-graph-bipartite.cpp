64975719be7ceced9cf272053eccc379d34729b6
Aman Singh Negi <159263291+aman-singh-negi@users.noreply.github.com>
2025-03-31 00:03:49 +0530

    Time: 0 ms (100%), Space: 17.1 MB (77.43%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
@@ -1,27 +1,31 @@
 class Solution {
-private:
-    bool dfs(vector<vector<int>>& graph, vector<int>& visited, int src) {
-        for (int neighbor : graph[src]) {
-            if (visited[neighbor] == -1) {
-                visited[neighbor] = 1 - visited[src];
-                if (!dfs(graph, visited, neighbor)) return false;
-            } 
-            else if (visited[neighbor] == visited[src]) {
-                return false;
+    private:
+        bool dfs(vector<vector<int>>&graph,vector<int>&visited,int src)
+        {
+            for(int i=0;i<graph[src].size();i++)
+            {
+                if(visited[graph[src][i]]==-1)
+                {
+                    visited[graph[src][i]]=1-visited[src];
+                    if(!dfs(graph,visited,graph[src][i]))return false;
+                }
+                else if(visited[graph[src][i]]==visited[src])
+                {
+                    return false;
+                }
             }
+            return true;
         }
-        return true;
-    }
-
 public:
     bool isBipartite(vector<vector<int>>& graph) {
-        int n = graph.size();
-        vector<int> visited(n, -1);
-
-        for (int i = 0; i < n; i++) {
-            if (visited[i] == -1) { 
-                visited[i] = 0;
-                if (!dfs(graph, visited, i)) return false;
+        int n=graph.size();
+        vector<int>visited(n,-1);
+        for(int i=0;i<n;i++)
+        {
+            if(visited[i]==-1)
+            {
+                visited[i]=0;
+                if(!dfs(graph,visited,i))return false;
             }
         }
         return true;
