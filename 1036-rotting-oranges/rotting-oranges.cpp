63d1baf534edfc1db66118544c86e9737f1006e7
Aman Singh Negi <159263291+aman-singh-negi@users.noreply.github.com>
2025-03-31 01:50:53 +0530

    Time: 1 ms (41.76%) | Memory: 16.8 MB (66.11%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -1,54 +1,53 @@
 class Solution {
-private:
-    void bfs(vector<vector<int>>&grid,int& cnt,queue<pair<int,int>>&q,int idx[],int idy[],int& fresh)
-    {
-        while(!q.empty())
+    private:
+        void bfs(vector<vector<int>>&grid,queue<pair<int,int>>&q,int& fresh,int& cnt,int idx[],int idy[])
         {
-            int size=q.size();
-            bool isOrange=false;
-            for(int j=0;j<size;j++)
+            while(!q.empty())
             {
-                pair<int,int>temp=q.front();
-                int sr=temp.first;
-                int sc=temp.second;
-                q.pop();
-                for(int i=0;i<4;i++)
+                int size=q.size();
+                bool flag=false;
+                for(int i=0;i<size;i++)
                 {
-                    int newSr=sr+idx[i];
-                    int newSc=sc+idy[i];
-                    if(newSr>=0 && newSr<grid.size() && newSc>=0 && newSc<grid[0].size() && grid[newSr][newSc]==1)
+                    pair<int,int>temp=q.front();
+                    q.pop();
+                    int ff=temp.first;
+                    int ss=temp.second;
+                    for(int j=0;j<4;j++)
                     {
-                        fresh--;
-                        q.push({newSr,newSc});
-                        grid[newSr][newSc]=2;
-                        isOrange=true;
+                        int new_x=ff+idx[j];
+                        int new_y=ss+idy[j];
+                        if(0<= new_x && new_x<grid.size() && new_y>=0 && new_y<grid[0].size() && grid[new_x][new_y]==1)
+                        {
+                            q.push({new_x,new_y});
+                            grid[new_x][new_y]=2;
+                            fresh--;
+                            flag=true;
+                        }
                     }
                 }
+                if(flag)cnt++;
             }
-            if(isOrange)cnt++;
         }
-    }
 public:
-    int orangesRotting(vector<vector<int>>& grid) 
-    {
-        int cnt=0;
-        int idx[4]={-1,0,+1,0};
-        int idy[4]={0,+1,0,-1};
+    int orangesRotting(vector<vector<int>>& grid) {
+        int n=grid.size();
         queue<pair<int,int>>q;
         int fresh=0;
-        for(int i=0;i<grid.size();i++)
+        int idx[4]={-1,0,+1,0};
+        int idy[4]={0,+1,0,-1};
+        for(int i=0;i<n;i++)
         {
             for(int j=0;j<grid[0].size();j++)
             {
-                if(grid[i][j]==1)fresh++;
-                if(grid[i][j]==2)
-                {
-                    q.push({i,j});
-                }
+                if(grid[i][j]==2)q.push({i,j});
+                else if(grid[i][j]==1)fresh++;
             }
         }
-        bfs(grid,cnt,q,idx,idy,fresh);
-        if(fresh)return -1;
+        if(q.empty() && fresh)return -1;
+        if(q.empty())return 0;
+        int cnt=0;
+        bfs(grid,q,fresh,cnt,idx,idy);
+        if(fresh>0)return -1;
         return cnt;
     }
 };
