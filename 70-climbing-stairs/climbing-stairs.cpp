0e6f7b29dd0028348215bd775f7cbe359a89ebb6
Aman Singh Negi <159263291+aman-singh-negi@users.noreply.github.com>
2025-05-09 12:46:42 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (17.25%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,16 +1,14 @@
 class Solution {
+private:
+    int memo(int n,vector<int>&v)
+    {
+        if(n<=1)return 1;
+        if(v[n]!=-1)return v[n];
+        return v[n]=memo(n-1,v)+memo(n-2,v);
+    }
 public:
     int climbStairs(int n) {
-        //Tabular Space Optimization
-        if(n<=2)return n;
-        int prev2=1;
-        int prev1=2;
-        for(int i=3;i<=n;i++)
-        {
-            int current=prev1+prev2;
-            prev2=prev1;
-            prev1=current;
-        }
-        return prev1;
+        vector<int>v(n+1,-1);
+        return memo(n,v);
     }
 };
