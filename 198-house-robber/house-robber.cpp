4b5b246aa721e642dd4ca143fc4e8638ffee3c90
Aman Singh Negi <159263291+aman-singh-negi@users.noreply.github.com>
2025-05-15 10:30:33 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (22.19%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -10,6 +10,15 @@ public:
     int rob(vector<int>& nums) {
         int n=nums.size();
         vector<int>dp(n+1,-1);
-        return find(nums,n,dp);
+        dp[0]=0;
+        for(int i=1;i<=nums.size();i++)
+        {
+            if(i==1)
+            {
+                dp[i]=nums[i-1]+dp[i-1];
+            }
+            else dp[i]=max(dp[i-1],nums[i-1]+dp[i-2]);
+        }
+        return dp[n];
     }
 };
