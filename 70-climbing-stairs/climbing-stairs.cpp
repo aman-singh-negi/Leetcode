d14e26b4b65cb38806b4bf1f54c18173f4a107c0
Aman Singh Negi <159263291+aman-singh-negi@users.noreply.github.com>
2025-05-15 10:22:35 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (17.17%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -9,13 +9,15 @@ private:
 public:
     int climbStairs(int n) {
         vector<int>dp(n+1,-1);
-        dp[0]=1;
-        dp[1]=1;
+        int prev0=1;
+        int prev1=1;
         if(n<=1)return 1;
         for(int i=2;i<=n;i++)
         {   
-            dp[i]=dp[i-1]+dp[i-2];
+            int temp=prev0+prev1;
+            prev0=prev1;
+            prev1=temp;
         }
-        return dp[n];
+        return prev1;
     }
 };
