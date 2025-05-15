353ec71c808376441e9bf951bd8d76c5c0c68169
Aman Singh Negi <159263291+aman-singh-negi@users.noreply.github.com>
2025-05-15 10:27:42 +0530

    Time: 1 ms (9.22%), Space: 10.6 MB (45.43%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,27 +1,15 @@
 class Solution {
 private:
-    int solve(vector<int>& nums,int n,vector<int>&dp)
+    int find(vector<int>&nums,int n,vector<int>&dp)
     {
-        //Memoization
         if(n<=0)return 0;
         if(dp[n]!=-1)return dp[n];
-        return dp[n]=max(nums[n-1] + solve(nums,n-2,dp), solve(nums,n-1,dp));
+        return dp[n]= max(nums[n-1]+find(nums,n-2,dp),find(nums,n-1,dp)); 
     }
 public:
     int rob(vector<int>& nums) {
-        //Tabulation
-        int n = nums.size();
-        if (n == 0) return 0;
-        if (n == 1) return nums[0];
-        int prev0=nums[0];
-        int prev1=max(nums[0],nums[1]);
-        int ans=0;
-        for (int i = 2; i < n; i++) {
-            ans= max(prev1, prev0 + nums[i]);
-            prev0=prev1;
-            prev1=ans;
-        }
-        return ans;
-    
-}
+        int n=nums.size();
+        vector<int>dp(n+1,-1);
+        return find(nums,n,dp);
+    }
 };
