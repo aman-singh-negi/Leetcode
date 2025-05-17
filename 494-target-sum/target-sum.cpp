da212d390533f8f7b67853101eafef0222f009eb
Aman Singh Negi <159263291+aman-singh-negi@users.noreply.github.com>
2025-05-17 09:25:26 +0530

    Time: 919 ms (8.87%) | Memory: 11.4 MB (98.63%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,17 +1,15 @@
 class Solution {
 private:
-    int find(vector<int>&nums,int target,int n,int sum,vector<vector<int>>&dp,int maxi)
+    int find(vector<int>&nums,int target,int n,int sum)
     {
-        if(target==sum && n==0)return 1;
+        if(target==0 && n==0)return 1;
         if(n==0)return 0;
-        if(dp[n][sum+maxi]!=-1)return dp[n][sum+maxi];
-        return dp[n][sum+maxi]= find(nums,target,n-1,sum+nums[n-1],dp,maxi) + find(nums,target,n-1,sum-nums[n-1],dp,maxi);
+        return find(nums,target-nums[n-1],n-1,sum+nums[n-1]) + find(nums,target+nums[n-1],n-1,sum-nums[n-1]);
     }
 public:
     int findTargetSumWays(vector<int>& nums, int target) {
+        //BruteForce Pure Recursive
         int n=nums.size();
-        int maxi=accumulate(nums.begin(),nums.end(),0);
-        vector<vector<int>>dp(n+1,vector<int>((2*maxi)+1,-1));
-        return find(nums,target,n,0,dp,maxi);
+        return find(nums,target,n,0);
     }
 };
