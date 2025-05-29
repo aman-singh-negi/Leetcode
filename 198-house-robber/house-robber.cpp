ac65e7b518f063a460fd735a9b50e09291e8b9ac
Aman Singh Negi <159263291+aman-singh-negi@users.noreply.github.com>
2025-05-29 11:20:54 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (89.94%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,19 +1,25 @@
 class Solution {
 private:
-    int find(vector<int>&nums,int n,int dp[])
-    {
-        if(n==0)return nums[n];
-        if(n<=0)return 0;
-        if(dp[n]!=-1)return dp[n];
-        return dp[n]=max(nums[n]+find(nums,n-2,dp),find(nums,n-1,dp));
-    }
+    // int find(vector<int>&nums,int n,int dp[])
+    // {
+    //     if(n==0)return nums[n];
+    //     if(n<=0)return 0;
+    //     if(dp[n]!=-1)return dp[n];
+    //     return dp[n]=max(nums[n]+find(nums,n-2,dp),find(nums,n-1,dp));
+    // }
 public:
     int rob(vector<int>& nums) 
     {
-        //Memoization
+        //Tabulation
         int n=nums.size();
         int dp[n+1];
         memset(dp,-1,sizeof(dp));
-        return find(nums,n-1,dp);
+        dp[0]=0;
+        dp[1]=nums[0];
+        for(int i=2;i<=n;i++)
+        {
+            dp[i]=max(nums[i-1]+dp[i-2],dp[i-1]);
+        }
+        return dp[n];
     }
 };
