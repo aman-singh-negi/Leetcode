831c77560e31286cc17613946e8a331aadf03516
Aman Singh Negi <159263291+aman-singh-negi@users.noreply.github.com>
2025-08-01 11:28:37 +0530

    Time: 1 ms (87.16%) | Memory: 15.7 MB (36.15%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.cpp
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
-    void f(vector<string>&ans,int open,int close,int n,string s)
+    void make(int open,int close,int n,vector<string>&ans,string s)
     {
-        if(s.size()==2*n)
+        if(s.length()==2*n)
         {
             ans.push_back(s);
-            return;
+        }   
+        if(open>close)
+        {
+            make(open,close+1,n,ans,s+')');
         }
-        if(open<n)f(ans,open+1,close,n,s+"(");
-        if(close<open)f(ans,open,close+1,n,s+")");
+        if(open<n)make(open+1,close,n,ans,s+'(');
     }
-    vector<string> generateParenthesis(int n) 
-    {
-        //Number of Open Brackets < N
-        //Number of Close Brackers < Number of Open Brackts
+    vector<string> generateParenthesis(int n) {
+        //your code goes here
         vector<string>ans;
-        f(ans,0,0,n,"");
+        make(0,0,n,ans,"");
         return ans;
     }
 };
