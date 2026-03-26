110814982811d9668190b26615641c170a494c27
Aman Singh Negi <159263291+aman-singh-negi@users.noreply.github.com>
2026-03-27 00:26:51 +0530

    Time: 1119 ms (41.54%) | Memory: 404.9 MB (46.15%) - LeetSync

---
+++ 3850-equal-sum-grid-partition-ii/equal-sum-grid-partition-ii.cpp
@@ -0,0 +1,65 @@
+class Solution {
+public:
+    bool solve(vector<vector<int>>& grid)
+    {
+        int n=grid.size();
+        int m=grid[0].size();
+        unordered_map<int,int> bottom, top;
+        long long bottomsum=0,topsum=0,maxi=INT_MIN;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(maxi<grid[i][j])maxi=grid[i][j];
+                bottomsum+=grid[i][j];
+                bottom[grid[i][j]]++;
+            }
+        }
+        for(int i=0;i<n-1;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                topsum+=grid[i][j];
+                top[grid[i][j]]++;
+                bottomsum-=grid[i][j];
+                bottom[grid[i][j]]--;
+                if(bottom[grid[i][j]]==0)bottom.erase(grid[i][j]);
+            }
+            //Conditions
+            if(topsum==bottomsum)return true;
+            long long diff=topsum-bottomsum;
+            if(diff>0 && abs(diff)<=maxi)
+            {
+                //Top section is greater
+                if(grid[0][0]==diff || grid[0][m-1]==diff)return true;
+                if(m==1 && diff==grid[i][0])return true;
+                if(i>0 && m>1  && top.count(diff))return true;
+            }
+            else if(abs(diff)<=maxi){
+                //Bottom section is Greater
+                diff=diff*(-1);
+                if(grid[i+1][0]==diff || grid[i+1][m-1]==diff)return true;
+                if(m==1 && grid[n-1][0]==diff)return true;
+                if((n-i-1)>1 && m>1 && bottom.count(diff))return true;
+            }
+        }
+        return false;
+    }
+    bool canPartitionGrid(vector<vector<int>>& grid) {
+        //Horizontal Cuts
+        if(solve(grid))return true;
+        int n=grid.size();
+        int m=grid[0].size();
+        vector<vector<int>>transpose(m,vector<int>(n));
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                transpose[j][i]=grid[i][j];
+            }
+        }
+        //Vertical Cuts
+        if(solve(transpose))return true;
+        return false;
+    }
+};
