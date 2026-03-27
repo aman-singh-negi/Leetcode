486fe4c8df8bde56399da40d3587c432a5d0ed2e
Aman Singh Negi <159263291+aman-singh-negi@users.noreply.github.com>
2026-03-27 14:07:03 +0530

    Time: 3 ms (29.93%) | Memory: 21.8 MB (46.17%) - LeetSync

--- 57-insert-interval/insert-interval.cpp
+++ 57-insert-interval/insert-interval.cpp
@@ -1,22 +1,12 @@
 class Solution {
 public: 
-    static bool comp(vector<int>&a,vector<int>&b)
-    {
-        if(a[0]<b[0])return true;
-        if(a[0]==b[0])
-        {
-            if(a[1]<b[1])return true;
-            return false;
-        }
-        return false;
-    }
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
         int n=intervals.size();
-        sort(intervals.begin(),intervals.end(),comp);
+        sort(intervals.begin(),intervals.end());
         int starttime=intervals[0][0];
         int endtime=intervals[0][1];
         vector<vector<int>>ans;
-        for(int i=0;i<n;i++)
+        for(int i=1;i<n;i++)
         {
             if(endtime>=intervals[i][0])
             {
@@ -25,19 +15,13 @@ public:
             }
             else
             {
-                if(starttime!=-1)
-                {
-                    ans.push_back({starttime,endtime});
-                    starttime=-1;
-                }
+                ans.push_back({starttime,endtime});
+                starttime=-1;
                 starttime=intervals[i][0];
                 endtime=intervals[i][1];
             }
         }
-        if(starttime!=-1)
-        {
             ans.push_back({starttime,endtime});
-        }
         return ans;
 
     }
