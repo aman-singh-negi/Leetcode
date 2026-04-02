ea83c23d92f5c9dcfbb74cd91725f4f7fe50cfab
Aman Singh Negi <159263291+aman-singh-negi@users.noreply.github.com>
2026-04-03 00:21:47 +0530

    Time: 43 ms (62.78%), Space: 94.1 MB (37.88%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
@@ -1,24 +1,21 @@
 class Solution {
-public: 
-    static bool comp(vector<int>a,vector<int>b)
+public:
+    static bool comp(vector<int>&a,vector<int>&b)
     {
-        if(a[1]<b[1])return true;
-        return false;
+        return a[1]<b[1];
     }
     int eraseOverlapIntervals(vector<vector<int>>& intervals) {
-        sort(intervals.begin(),intervals.end(),comp);
-        int endtime=INT_MIN;
         int cnt=0;
+        sort(intervals.begin(),intervals.end(),comp);
+        int endPoint=INT_MIN;
         for(int i=0;i<intervals.size();i++)
         {
-            if(intervals[i][0]<endtime)
+            if(endPoint>intervals[i][0])
             {
                 cnt++;
+                continue;
             }
-            else
-            {
-                endtime=intervals[i][1];
-            }
+            endPoint=max(endPoint,intervals[i][1]);
         }
         return cnt;
     }
