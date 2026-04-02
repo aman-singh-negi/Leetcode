381aa61fb343c70fea5d9bc4d4456bece550962b
Aman Singh Negi <159263291+aman-singh-negi@users.noreply.github.com>
2026-04-02 13:17:07 +0530

    Time: 604 ms (36.17%), Space: 275.9 MB (55.32%) - LeetHub

---
+++ 3418-maximum-amount-of-money-robot-can-earn/3418-maximum-amount-of-money-robot-can-earn.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    int maximumAmount(vector<vector<int>>& coins) {
+        int m = coins.size();
+        int n = coins[0].size();
+        vector<vector<vector<int>>> dp(m, vector<vector<int>>(n, vector<int>(3, INT_MIN)));
+        // Base case
+        for(int k = 0; k < 3; k++) {
+            dp[0][0][k] = coins[0][0];
+            if(coins[0][0] < 0 && k > 0) dp[0][0][k] = 0; // skip
+        }
+
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if(i == 0 && j == 0) continue;
+
+                for(int k = 0; k < 3; k++) {
+
+                    int best = INT_MIN;
+
+                    // from top
+                    if(i > 0) best = max(best, dp[i-1][j][k]);
+                    // from left
+                    if(j > 0) best = max(best, dp[i][j-1][k]);
+
+                    // take coin
+                    if(best != INT_MIN)
+                        dp[i][j][k] = max(dp[i][j][k], best + coins[i][j]);
+
+                    // skip negative
+                    if(coins[i][j] < 0 && k > 0) {
+                        int skip_best = INT_MIN;
+
+                        if(i > 0) skip_best = max(skip_best, dp[i-1][j][k-1]);
+                        if(j > 0) skip_best = max(skip_best, dp[i][j-1][k-1]);
+
+                        dp[i][j][k] = max(dp[i][j][k], skip_best);
+                    }
+                }
+            }
+        }
+
+        return max({dp[m-1][n-1][0], dp[m-1][n-1][1], dp[m-1][n-1][2]});
+    }
+};
