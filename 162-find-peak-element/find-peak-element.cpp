2b81cc7da7d5cfb05b057b8b77b1c741b5f38969
Aman Singh Negi <159263291+aman-singh-negi@users.noreply.github.com>
2026-04-10 01:51:25 +0530

    Time: 0 ms (100.00%) | Memory: 12.6 MB (39.32%) - LeetSync

--- 162-find-peak-element/find-peak-element.cpp
+++ 162-find-peak-element/find-peak-element.cpp
@@ -1,18 +1,31 @@
 class Solution {
 public:
-    int findPeakElement(vector<int>& arr) {
-        int low=1,high=arr.size()-2;
-        if(arr.size()==1)return 0;
-        if(arr[0]>arr[1])return 0;
-        if(arr[arr.size()-1]>arr[arr.size()-2])return arr.size()-1;
+    int findPeakElement(vector<int>& nums) {
+        int n=nums.size();
+        int low=1,high=n-2;
+        if(n==0)return 0;
+        if(n==1)return 0;
+        if(nums[0]>nums[1])return 0;
+        if(nums[n-1]>nums[n-2])return n-1;
         while(low<=high)
         {
-            int mid=low +(high-low)/2;
-            if(arr[mid-1]< arr[mid] && arr[mid+1]<arr[mid])return mid;
-            else if(arr[mid-1]<arr[mid])low=mid+1;
-            else if(arr[mid+1]<arr[mid])high=mid-1;
-            else low=mid+1;
+            int mid=(low+high)/2;
+            if(nums[mid]>nums[mid-1] && nums[mid]>nums[mid+1])return mid;
+            else if(nums[mid]>nums[mid-1])
+            {
+                //Uphill
+                low=mid+1;
+            }
+            else if(nums[mid]>nums[mid+1])
+            {
+                //DownHill
+                high=mid-1;
+            }
+            else
+            {
+                low=mid+1;
+            }
         }
         return -1;
-    }
+    }   
 };
